8e082ba5adef434b1ae5f845ce8df804da566395
Andrew Rust <rustac19@wfu.edu>
2021-09-21 23:17:01 -0400

    almost done, but having trouble printing items in list, all else is completely functional.

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -7,16 +7,17 @@ import java.util.*;
 public class DLinkedList<T> implements List<T>, Iterable<T> {
     /**
      * This method overrides the addfirst method of the List class and allows the user to add an element to their DLinkedList
-     * @param item
+     * @param data
      */
     @Override
-    public void addFirst(T item) { //This adds an item to the front of the DLinked List
-        Node newNode = new Node(item, head, null);
-        if (head == null){ //if the list is empty
-            tail = head = newNode; //then both head and tail point to the first node ('newNode')
-        } else{ // otherwise head moves to second spot and new node becomes head
-            head = head.next;
-            newNode = head;
+    public void addFirst(T data) { //This adds an item to the front of the DLinked List
+        head = new Node(data, head, null);// create new node for head with data of input
+        Node second = head.next; //create node of second being the next node after head,
+        if (second != null){ // if second index is null, set head to the previous of second
+            second.prev = head;
+        }
+        else {
+            tail = head; // otherwise this is an empty list node and head is the same as tail
         }
     }
 
@@ -27,17 +28,20 @@ public class DLinkedList<T> implements List<T>, Iterable<T> {
      */
     @Override
     public boolean contains(T item) {
-        Boolean result = false; //creating variable for result of search
-        Iterator<T> it = iterator(); // creating 'iterator' object
-        while (it.hasNext()){ //checking if iterator has a next item
-            if (it.next() == item){ //checking if next item is in face the item in question
-                result = true; //if it is, set return var to 'true'
-            }
-            else {
-                result = false; //otherwise, it's false since that is not the item in question
-            }
+        Boolean result = false; //set result var Boolean to be false
+        if (head == null) { //if the head is empty, the list is empty, so it doesnt contain anything, so return false
+            return false;
+        } else if (head.data.equals(item)) { //if the first value is the same as the value being searched for then the method returns true
+            result = true;
         }
-        return result; //return statement
+        Node temp = head; // temporary node
+        while (temp.next != null) { // iterate through to check all data if there is any that match the inputted item
+            if (temp.data == item) {
+                result = true;
+            }
+            temp = temp.next;
+        }
+        return result;
     }
 
     /**
@@ -47,8 +51,8 @@ public class DLinkedList<T> implements List<T>, Iterable<T> {
     @Override
     public int size() {
         int count = 0; //creating count var to be size of DLinkedList
-        Iterator<T> it = iterator(); // creating 'iterator' object
-        while(it.hasNext()){ //while the iterator has the next value...
+         // creating 'iterator' object
+        while(iterator().hasNext()){ //while the iterator has the next value...
             count++; //add 1 to the count
         }
         return count; //once the iterator has no more values, return count to output the size of the list
@@ -60,66 +64,12 @@ public class DLinkedList<T> implements List<T>, Iterable<T> {
      */
     @Override
     public boolean isEmpty() {
-        Iterator<T> it = iterator(); // creating 'iterator' object
-        if (it.hasNext()){//if the iterator does not have a next value, then the list is empty, thus, we return true
-            return true;
-        } else{ //otherwise, the list is not empty and we return false
-            return false;
+
+        if (head == null) {//check if list is empty or the first value is null
+            return true; // the list is empty
+        } else {
+            return false;//otherwise the list is not empty, return false
         }
-    }
-
-    /**
-     * overrides iterator method of Iterator class method
-     * @return object of T class from Iterator
-     */
-    @Override
-    public Iterator<T> iterator() {
-        return new ListIterator<T>() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
-
-            @Override
-            public T next() {
-                return null;
-            }
-
-            @Override
-            public boolean hasPrevious() {
-                return false;
-            }
-
-            @Override
-            public T previous() {
-                return null;
-            }
-
-            @Override
-            public int nextIndex() {
-                return 0;
-            }
-
-            @Override
-            public int previousIndex() {
-                return 0;
-            }
-
-            @Override
-            public void remove() {
-
-            }
-
-            @Override
-            public void set(T t) {
-
-            }
-
-            @Override
-            public void add(T t) {
-
-            }
-        };
     }
 
     private class Node{
@@ -135,11 +85,40 @@ public class DLinkedList<T> implements List<T>, Iterable<T> {
     }
     T data;
     Node head;
-    Node tail;  
+    Node tail;
 
     public DLinkedList() {
         head = null;
         tail = null;
     }
-    
+    /**
+     * overrides iterator method of Iterator class method
+     * @return object of T class from Iterator
+     */
+    public class DListIterator extends DLinkedList<T> implements Iterator<T>{//this is a class of the iterator so the methods of the iterator can be used
+        private Node nextNode = head;
+        @Override
+        public boolean hasNext() {
+            return nextNode != null;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Iter exceeded");
+            }
+            T data = nextNode.data;
+            nextNode = nextNode.next;
+            return data;
+        }
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new DListIterator();
+    }
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
+
--- src/ProcessFQDN.java
+++ src/ProcessFQDN.java
@@ -10,8 +10,8 @@
  */
 
 import java.io.*;
+import java.util.Locale;
 import java.util.Scanner;
-import java.util.regex.*;
 
 public class ProcessFQDN {
 
@@ -24,11 +24,8 @@ public class ProcessFQDN {
         if(!argsOK(args))
             System.exit(1);
         readNameFile(args[0]);
-
-
-        //for (String : test.iterator().hasNext())
-        //{ System.out.println("value" + test);} ;
     }
+
     /**
      *  This method reads (and eventually stores) FQDN and 2LD in the fqdn file
      *
@@ -36,70 +33,125 @@ public class ProcessFQDN {
      */
     static void readNameFile(String fileName){
         System.out.println("Processing " + fileName);
+
         // reading dictionary file
-        int fullyQDN = 0;// integer of the number of fully qualified domain names
-        DLinkedList<String> FQDNList = new DLinkedList<String>(); //DLinked list for each of the different fully qualified domain names
-        DLinkedList<String> TwoLDList = new DLinkedList<String>();
-        DLinkedList<String> TLDList = new DLinkedList<String>();
-
-        DLinkedList<String> list = new DLinkedList<String>(); //Added DLinkedList object called 'list' to store values of the input file
-
         try {
             Scanner input = new Scanner(new File(fileName));
+            int uniqueFQDNs = 0; // counting integer for each tag of valid FQDNs
+            int unique2LDs = 0;
+            int uniqueTLDs = 0;
+            int numFQDNs = 0;
+
+            DLinkedList<String> listFQDN = new DLinkedList<>(); //DLinked list for each of the different fully qualified domain names
+            DLinkedList<String> list2LDs = new DLinkedList<>();
+            DLinkedList<String> listTLDs = new DLinkedList<>();
             while (input.hasNext()) {
                 String line = input.nextLine();
-                System.out.println("debug: " + line);
-                list.addFirst(line);// populate 'list' DLinkedList array
+                line = line.toLowerCase(); // convert all input lines to lowercase
+
+                if (validateDomain(line)) { // calling validation method to check if its a legitimate domain
+                    numFQDNs++; // adding to total number of legitimate domains
+                    if (!(listFQDN.contains(line))) { // if it is not already inside the list of valid FQDNs, then add it
+                        uniqueFQDNs++;
+                        listFQDN.addFirst(line); // add to list of FQDNS
+                        if (!list2LDs.contains(removeTopDomains(line))) { // break down into 2LDs and add to list if its not already in it
+                            unique2LDs++; //add to number of 2LDs
+                            list2LDs.addFirst(removeTopDomains(line));//add 2LD to list
+                        }
+                        if (!listTLDs.contains(removeLastDomain(line))) {// break down into TLDs and add to list if its not already in it
+                            uniqueTLDs++; //add to number of TLDs
+                            listTLDs.addFirst(removeLastDomain(line)); //add TLD to list
+                        }
+                    }
                 }
+
+            }
             input.close();
+            // Print statements that meet desired formatting
+            System.out.println("Found " + numFQDNs + " FQDNs, " + uniqueFQDNs + " unique FQDNs, " + unique2LDs + " unique 2LDs, "+ uniqueTLDs + " unique TLDs");
+            displayNameInfo(listFQDN, list2LDs,listTLDs); // call method to print list of items
         }
         catch (FileNotFoundException e) {
             System.out.println("Error in opening " + fileName);
             System.exit(1);
         }
-        int totalFQNS = 0;
-        int uniqueFQDNs = 0;
-        int TwoLDs = 0;
-        int TLDs = 0;
-        String regexFQDN = "^([A-Za-z]{1, 63}\\.)+[A-Za-z]{1, 63}\\.)+[A-Za-z]{1, 63}$";
-        String regex2DL = "^([A-Za-z]{1, 63}\\.)+[A-Za-z]{1, 63}$";
-        for (String domainName : list){
 
-            if ((domainName.contains("."))){
-                domainName.split()
-            } else if ()
-                domainName
+    }
+
+    /**
+     * Validating Domain Method
+     * @param in is the String to be checked for validity
+     * @return a boolean if it is valid or not
+     */
+    public static Boolean validateDomain(String in){
+        Boolean result = null; // set intial result Boolean to null, depending on if any of the tests fail, this variable will be updated
+        String[] subDomains = in.split("\\."); // split input into array of strings on every '.'
+        for (String label : subDomains){ //if any one string exceed 63 chars, it fails
+            if (label.length() > 63){
+                result = false;
+            }
         }
-
-        for (String string : list)
-            System.out.println(string);
+        if (subDomains.length <= 1){ // if there is only one element in the array, meaning that the input either has no period or it does not have elements on either side, it fails
+            result = false;
+        } else if (in.charAt(0) == '.') { // if it begins with a '.' it fails
+            result = false;
+        } else if (in.charAt(in.length() - 1) == '.'){ // if it ends with a '.' it fails
+            result = false;
+        } else if (in.length() > 253) { // if the entire list is too long, it fails
+            result = false;
+        } else { // if it passes all those tests, it passes as a valid FQDN
+            result = true;
+        }
+        return result;
     }
-    public void analyseFile(){
 
+    /**
+     * Remove Top domains of valid FQDN, all but last two to create valid 2LD
+     * @param in String to be chopped down into 2LD
+     * @return returns valid 2LD as String
+     */
+    public static String removeTopDomains(String in){
+        String[] subDomains = in.split("\\."); // create array to split into subdomains
+        int length = subDomains.length; // integer for length
+        String newDomain = subDomains[length-2] + "." + subDomains[length-1]; // creating valid 2LD from last two indicies in subdomain array
+        return newDomain;
     }
 
+    /**
+     * Remove first of last two labels in valid 2LDs to create valid TLD
+     * @param in String to be chopped down in valid TLD
+     * @return returns valid 2LD as String
+     */
+    public static String removeLastDomain(String in){
+        String[] subDomains = in.split("\\."); // create array to split into subdomains
+        int length = subDomains.length; // integer for array length
+        String newDomain = subDomains[length-1]; // creating valid TLD from last index in subdomain array
+        return newDomain;
+    }
     /**
      *  This method displays the stats and FQDN and 2LD found in the file
      *
      *  @param DLinkedLists for each of the three printable collections (FQDNs, 2LDs, and TLDs)
      */
-    static void displayNameInfo(DLinkedList FDDNs, DLinkedList TwoLDs, DLinkedList TLDs) {
 
-        System.out.print("Unique FQDNs: ");
-        for (Object string: FDDNs){
-            System.out.print(FDDNs.iterator().next() + " ");
+    static void displayNameInfo(DLinkedList<String> FDDNs, DLinkedList<String> TwoLDs, DLinkedList<String> TLDs) {
+
+        System.out.print("Unique FQDNs: "); // enhanced for loop to print out each array, print statements are used according to desired formatting of assignment
+        for (String data: FDDNs){
+            System.out.print(data + " ");
         }
+        System.out.println();
         System.out.print("Unique 2LDs:  ");
-        for (Object string: TwoLDs){
-            System.out.print(TwoLDs.iterator().next() + " ");
+        for (String data: TwoLDs){
+            System.out.print(data + " ");
         }
+        System.out.println();
         System.out.print("Unique TLDs:  ");
-        for (Object string: TLDs){
-            System.out.print(TLDs.iterator().next() + " ");
+        for (String data: TLDs){
+            System.out.print(data + " ");
         }
+        System.out.println();
     }
-
-
     /**
      *  This method returns true if the command line arguments are acceptable,
      *  1 file name (String) must exist in args
@@ -114,28 +166,5 @@ public class ProcessFQDN {
         }
         return true;
     }
-/* DEAD Code
-    /**
-     * Method using regular expression in java to check if domain name is legitimate or matches the given criteria
-     * @param domain is the domain name given from the DLinkedList object 'list'
-     * @return int
 
-    static int domainValidation(String domain){
-        String regex2DL = "^([A-Za-z]{1,63}\\.)+[A-Za-z]{1,63}$"; // regular expression for 2LDs
-        Pattern pattern2DL = Pattern.compile(regex2DL);
-        Matcher check2Dl = pattern2DL.matcher(domain);
-
-        String regexFQDN = "^([A-Za-z]{1,63}\\.)+[A-Za-z]{1,63}\\.)+[A-Za-z]{1,63}$"; // regular expression for FQDN
-        Pattern patternFQDN = Pattern.compile(regexFQDN);
-        Matcher checkFQDN = patternFQDN.matcher(domain);
-
-        if (checkFQDN.matches()){
-            return 2;
-        } else if (check2Dl.matches()){
-            return 1;
-        } else{
-            return 0;
-        }
-    }
-*/
 }
